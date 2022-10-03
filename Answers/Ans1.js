9ac88bb93d7c469b2fad295edf730b5c255de59f
Mayank Budhiraja <cu.17bcs1192@gmail.com>
2022-10-03 16:51:26 +0530

    Completed Problem 1

---
+++ Answers/Ans1.js
@@ -0,0 +1,8 @@
+// For the Title Change
+document.querySelector(
+  ".side-bar .crayons-card .crayons-subtitle-2"
+).innerText = "INeuron";
+
+// For the Paragragh Change
+document.querySelector(".side-bar .crayons-card .color-base-70").innerText =
+  "I Write Code";
