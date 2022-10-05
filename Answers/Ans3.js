1f259836d7490fd8d33a92c8b9953f5ee62c6101
Mayank Budhiraja <cu.17bcs1192@gmail.com>
2022-10-05 23:18:08 +0530

    added answers for 3 and 4

---
+++ Answers/Ans3.js
@@ -0,0 +1,9 @@
+let navList = document.querySelector(".accordion-homepage");
+
+let newSection = document.createElement("section");
+
+const textNode = document.createTextNode("My New Faq");
+
+// newSection.appendChild(textNode);
+
+navList.appendChild(newSection);
---
+++ Answers/Ans4.js
@@ -0,0 +1,3 @@
+// Finding and Changing the Element
+
+document.querySelector(".one-tel-number").textContent = "+91 6366256689";
